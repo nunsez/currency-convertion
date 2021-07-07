5313efbae818fa34a6fd73df506ef77dead63145
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-07-07 14:37:57 +0700

    improve nav

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 
 const App = () => (
-    <div className="container w-50 min-vw-sm-100 py-3 bg-light">
+    <div className="container w-50 min-vw-sm-100 mb-3 pb-1 bg-light">
         <Header />
         <Main />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Header = () => (
-    <nav>
-        <ul>
-            <li><Link to="/">Converter</Link></li>
-            <li><Link to="/rates">Rates</Link></li>
-        </ul>
-    </nav>
+    <Navbar sticky="top" bg="light" className="border-bottom border-2 border-secondary py-3 mb-4">
+        <Navbar.Brand className="me-4">Navigation</Navbar.Brand>
+
+        <Nav className="d-flex justify-content-center">
+            <Nav.Link as={Link} to="/">Converter</Nav.Link>
+            <Nav.Link as={Link} to="/rates">Rates</Nav.Link>
+        </Nav>
+    </Navbar>
 );
 
 export default Header;
