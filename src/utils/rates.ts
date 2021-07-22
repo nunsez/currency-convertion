35fb32d1313c587b54b995360e12994ab11aca38
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-07-22 13:04:58 +0700

    conmact code

--- src/utils/rates.ts
+++ src/utils/rates.ts
@@ -2,13 +2,9 @@ import axios from "axios";
 
 import { IDaily, IRates } from "../interfaces";
 
-const dailyURL = 'https://www.cbr-xml-daily.ru/daily_json.js';
-
-const ratesURL = 'https://www.cbr-xml-daily.ru/latest.js';
-
 const URLMap: { [key: string]: string } = {
-    daily: dailyURL,
-    rates: ratesURL,
+    daily: 'https://www.cbr-xml-daily.ru/daily_json.js',
+    rates: 'https://www.cbr-xml-daily.ru/latest.js',
 };
 
 const getData = async (opt: string) => {
